d27b5129874a2e605803bf75006d62da91b33c20
KRISHAN2310 <krishangopal1394@gmail.com>
2026-04-04 06:46:16 +0530

    Change API base URL to production endpoint

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://golf-charity-subscription-platform-h98e.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
